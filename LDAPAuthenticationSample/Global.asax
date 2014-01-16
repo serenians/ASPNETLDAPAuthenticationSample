721fdea36962e4752874244a2ac6ee7909eee875
serenians <serenians@gmail.com>
2014-01-16 22:18:58 +0545

    Initial configuration

---
+++ LDAPAuthenticationSample/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LDAPAuthenticationSample.MvcApplication" Language="C#" %>
---
+++ LDAPAuthenticationSample/Global.asax.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Security.Principal;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Web.Security;
+
+namespace LDAPAuthenticationSample
+{
+    // Note: For instructions on enabling IIS6 or IIS7 classic mode,
+    // visit http://go.microsoft.com/?LinkId=9394801
+
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+
+            WebApiConfig.Register(GlobalConfiguration.Configuration);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        private void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+            string cookieName = FormsAuthentication.FormsCookieName;
+            HttpCookie authCookie = Context.Request.Cookies[cookieName];
+
+            if (null == authCookie)
+            {
+                //There is no authentication cookie.
+                return;
+            }
+            FormsAuthenticationTicket authTicket = null;
+            try
+            {
+                authTicket = FormsAuthentication.Decrypt(authCookie.Value);
+            }
+            catch (Exception ex)
+            {
+                //Write the exception to the Event Log.
+                return;
+            }
+            if (null == authTicket)
+            {
+                //Cookie failed to decrypt.
+                return;
+            }
+            //When the ticket was created, the UserData property was assigned a
+            //pipe-delimited string of group names.
+            string[] groups = authTicket.UserData.Split(new char[] { '|' });
+            //Create an Identity.
+            GenericIdentity id = new GenericIdentity(authTicket.Name, "LdapAuthentication");
+            //This principal flows throughout the request.
+            GenericPrincipal principal = new GenericPrincipal(id, groups);
+            Context.User = principal;
+        }
+    }
+}
